ca486b45ab9c7cba9fa712fdd7c6b3acf25a7689
jig10 <jigodoyrubin@gmail.com>
2019-12-14 16:59:26 +0000

    second commit

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from "react";
 import getState from "./flux.js";
-
 // Don't change, here is where we initialize our context, by default its just going to be Null.
-export const { Consumer, Provider } = React.createContext(null);
-
+export const Context = React.createContext(null);
 // This function injects the global store to any view/component where you want to use it, we will inject the context to Layout.jsx, you can see it here:
 // https://github.com/4GeeksAcademy/react-hello-webapp/blob/master/src/js/layout.jsx#L35
 const injectContext = PassedComponent => {
@@ -20,7 +18,6 @@ const injectContext = PassedComponent => {
 					})
 			})
 		);
-
 		useEffect(() => {
 			/**
 			 * EDIT THIS!
@@ -31,18 +28,29 @@ const injectContext = PassedComponent => {
 			 *
 			 **/
 			state.actions.loadAgendaSlug();
-		}, []);
 
+			let url = "https://assets.breatheco.de/apis/fake/contact/agenda/downtown_xii";
+			let promis = fetch(url).then(response => response.json());
+			promis.then(results => {
+				let { store, actions } = state;
+				setState({
+					...state,
+					store: {
+						...store,
+						contacts: results
+					}
+				});
+			});
+		}, []);
 		// the initial value for the context its not null anymore, but the current state of this component,
 		// the context will have a getStore and setStore functions available then, because they were declared
 		// on the state of this component
 		return (
-			<Provider value={state}>
+			<Context.Provider value={state}>
 				<PassedComponent {...props} />
-			</Provider>
+			</Context.Provider>
 		);
 	};
 	return StoreWrapper;
 };
-
 export default injectContext;
