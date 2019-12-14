26d0e6fdcd34fd30b812cf9d567bd2429c755a42
jig10 <jigodoyrubin@gmail.com>
2019-12-14 16:20:32 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,25 @@
-const getState = ({ getStore, setStore }) => {
+const getState = ({ getStore, setStore, getActions }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			loadAgendaSlug() {
+				const url = "https://assets.breatheco.de/apis/fake/contact/agenda/downtown_xii";
+
+				fetch(url)
+					.then(res => res.json())
+					.then(result => {
+						console.log("kkkkkkkkkkkkk", result);
+						setStore({
+							contacts: result
+						});
+					})
+					.catch(e => console.error(e));
+			}
 		}
 	};
 };
